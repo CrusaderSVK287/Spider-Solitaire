0ef9af3699d04c95159a7ca4f77c6319ebeedb01
Lukáš Belán <lukas.belan@student.tuke.sk>
2022-07-05 01:25:34 +0200

    declared enum stattype

---
+++ Statistics.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spider_Solitaire
+{
+    //members are in the same order as in file with statistics
+    public enum StatisticType
+    {
+        //general stats
+
+        GamesStarted,
+        GamesWon,
+        CardsMoved,
+        SuitsAssembled,
+        HintsTaken,
+
+        //1 suit games
+
+        OneSuitGamesStarted,
+        OneSuitGamesWon,
+        OneSpadeSuitsAssembled,
+        OneWinPercentage,
+
+        //2 suit games
+
+        TwoSuitGamesStarted,
+        TwoSuitGamesWon,
+        TwoSpadeSuitsAssembled,
+        TwoHeartsSuitsAssembled,
+        TwoWinPercentage,
+
+        //4 suit games
+
+        FourSuitGamesStarted,
+        FourSuitGamesWon,
+        FourSpadeSuitsAssembled,
+        FourHeartsSuitsAssembled,
+        FourClubSuitsAssembled,
+        FourDiamondsSuitsAssembled,
+        FourWinPercentage
+    }
+
+    //handels updates of stats
+    public abstract class Statistics
+    {
+        //increase general stats
+        public static void GamesStarted()
+        {
+            
+        }
+    }
+}
