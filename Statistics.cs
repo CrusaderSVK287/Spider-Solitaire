6c467c8142c0d780249aecd29102779c3911f157
Lukáš Belán <lukas.belan@student.tuke.sk>
2022-07-05 01:39:44 +0200

    Increase stat and reset stats methods

--- Statistics.cs
+++ Statistics.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
 
 namespace Spider_Solitaire
 {
@@ -11,45 +13,62 @@ namespace Spider_Solitaire
     {
         //general stats
 
-        GamesStarted,
-        GamesWon,
-        CardsMoved,
-        SuitsAssembled,
-        HintsTaken,
+        GamesStarted,   //0 number of total started games
+        GamesWon,       //1 number of total won games
+        CardsMoved,     //2 number of all the cards that were moved
+        SuitsAssembled, //3 number of total suits that have been assembled
+        SuitSpadesAssembled,    //4 number of total spade suits assembled
+        SuitHeartsAssembled,    //5 number of total Hearts suits assembled
+        SuitClubsAssembled,     //6 number of total Clubs suits assembled
+        SuitDiamondsAssembled,  //7 number of total diamonds suits assembled
+        HintsTaken,     //8 number of total hints taken
 
         //1 suit games
 
-        OneSuitGamesStarted,
-        OneSuitGamesWon,
-        OneSpadeSuitsAssembled,
-        OneWinPercentage,
+        OneSuitGamesStarted,    //9 number of games started with 1 suit
+        OneSuitGamesWon,        //10 number of games won with 1 suit
+        OneWinPercentage,       //11 percantage of winned 1 suit games
 
         //2 suit games
 
-        TwoSuitGamesStarted,
-        TwoSuitGamesWon,
-        TwoSpadeSuitsAssembled,
-        TwoHeartsSuitsAssembled,
-        TwoWinPercentage,
+        TwoSuitGamesStarted,    //12
+        TwoSuitGamesWon,        //13
+        TwoWinPercentage,       //14
 
         //4 suit games
 
-        FourSuitGamesStarted,
-        FourSuitGamesWon,
-        FourSpadeSuitsAssembled,
-        FourHeartsSuitsAssembled,
-        FourClubSuitsAssembled,
-        FourDiamondsSuitsAssembled,
-        FourWinPercentage
+        FourSuitGamesStarted,   //15
+        FourSuitGamesWon,       //16
+        FourWinPercentage       //17
     }
 
     //handels updates of stats
     public abstract class Statistics
     {
         //increase general stats
-        public static void GamesStarted()
+        public static void IncreaseStat(StatisticType type, int value)
         {
-            
+            if (!File.Exists(@"statistics.txt") || type==StatisticType.OneWinPercentage ||
+                                                   type==StatisticType.TwoWinPercentage ||
+                                                   type==StatisticType.FourWinPercentage) return;
+            try
+            {
+                var lines = File.ReadAllLines(@"statistics.txt");
+                lines[(int)type] = (Convert.ToInt32(lines[(int)type]) + value).ToString();
+                File.WriteAllLines(@"statistics.txt",lines);
+            }
+            catch (Exception e) { MessageBox.Show(e.ToString(),"Error",MessageBoxButton.OK,MessageBoxImage.Error); return; }
+        }
+
+        public static void ResetStatistics()
+        {
+            try
+            {
+                string[] lines = new string[18];
+                for (int i = 0; i < 18; i++) { lines[i] = "0"; }
+                File.WriteAllLines(@"statistics.txt", lines);
+            }
+            catch (Exception e) { MessageBox.Show(e.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error); return; }
         }
     }
 }
