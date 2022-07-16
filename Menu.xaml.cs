b4ee8c26f8ad761a5a0a888d1aee0565459a2595
Lukáš Belán <lukas.belan@student.tuke.sk>
2022-07-16 11:49:29 +0200

    made a how to play text code a liiitle bit more readable

--- Menu.xaml.cs
+++ Menu.xaml.cs
@@ -79,22 +79,13 @@ namespace Spider_Solitaire
         private void HowToPlayClick(object sender, RoutedEventArgs e)
         {
             SPInformation.Children.Clear();
-
-            TextBlock HowToPlay = new()
-            {
-                Text = "How to play",
-                FontSize = 40
-            };
-            SPInformation.Children.Add(HowToPlay);
-
-            TextBlock a = new()
-            {
-                Text = "The goal of spider solitaire is, as with all solitaires, to clear the entire deck. This is done by assembling a full suit of cards. " +
+            string text = "How to play";
+            SPInformation.Children.Add(new TextBlock() { Text = text, FontSize = 40 });
+            text = "The goal of spider solitaire is, as with all solitaires, to clear the entire deck. This is done by assembling a full suit of cards. " +
                 "Each suit consists of 13 cards ranging from ace to king, having the same colour. In order to assemble a suit, all 13 cards must be in order. " +
-                "Then the suit is removed from the deck, once all 8 suits are assembled, the game ends in victory. Below are all 4 kinds of suits\n(from the left: clubs, diamonds, spades, hearts)",
-                TextWrapping = TextWrapping.Wrap,
-            };
-            SPInformation.Children.Add(a);
+                "Then the suit is removed from the deck, once all 8 suits are assembled, the game ends in victory. Below are all 4 kinds of suits\n(from the left: clubs, diamonds, spades, hearts)";
+
+            SPInformation.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap });
 
             StackPanel b = new()
             {
@@ -112,15 +103,11 @@ namespace Spider_Solitaire
                 b.Children.Add(img);
             }
 
-            TextBlock c = new()
-            {
-                Text = "When selecting the number of suits from the main menu, you are selecting how many suits (or colours) will be used in game. This has no " +
+            text = "When selecting the number of suits from the main menu, you are selecting how many suits (or colours) will be used in game. This has no " +
                 "effect on the total number of cards. The number of total cards is always 104 (8 suits times 13 cards in one suit)\n\n" +
                 "To select a card or cards, click on the image of the card you want to select, an animation will show you which card has been selected. You can then click on a column where you would like to place them. If you cannot place them in the selected column or you selected the very same column, the cards will return to their original place. In the game, you are allowed to move only cards that are facing upwards (you can see the suit and value), and you can move this pile only on a card that follows the ascention order " +
-                "(7 can only go on top of 8, 3 on top of 4 etc..). You can, however, place a cards of different suit on top of each other. For reference, here is an image of a full suit of spades.",
-                TextWrapping = TextWrapping.Wrap,
-            };
-            SPInformation.Children.Add(c);
+                "(7 can only go on top of 8, 3 on top of 4 etc..). You can, however, place a cards of different suit on top of each other. For reference, here is an image of a full suit of spades.";
+            SPInformation.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap });
 
             StackPanel d = new()
             {
@@ -131,7 +118,7 @@ namespace Spider_Solitaire
             {
                 Image img = new()
                 {
-                    Width = (C==1)?89:25,
+                    Width = (C == 1) ? 89 : 25,
                     Height = 120,
                     Stretch = Stretch.None,
                     Source = new BitmapImage(new Uri(@"assets/" + C.ToString() + "c.png", UriKind.Relative)),
@@ -139,14 +126,10 @@ namespace Spider_Solitaire
                 d.Children.Add(img);
             }
 
-            TextBlock f = new()
-            {
-                Text = "When you move cards away and a uncovered (blue) card becomes the top most one, it reveals it's suit and value. " +
+            text = "When you move cards away and a uncovered (blue) card becomes the top most one, it reveals it's suit and value. " +
                 "Once you clear out an entire column such as there are no cards remaining and you can see a green outline, that means you can place any card or sequence of cards" +
-                " in there, refer to the outline image below.",
-                TextWrapping = TextWrapping.Wrap,
-            };
-            SPInformation.Children.Add(f);
+                " in there, refer to the outline image below.";
+            SPInformation.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap });
 
             Image g = new()
             {
@@ -156,14 +139,11 @@ namespace Spider_Solitaire
                 Source = new BitmapImage(new Uri(@"assets/card_outline.png", UriKind.Relative)),
             };
             SPInformation.Children.Add(g);
-            TextBlock h = new()
-            {
-                Text = "Since the biggest card is a King, it means that Kings can only be moved into empty columns. On the other hand, since aces are the smallest card," +
+
+            text = "Since the biggest card is a King, it means that Kings can only be moved into empty columns. On the other hand, since aces are the smallest card," +
                 " no card can be places on them. Once you run out of possible moves, you can deal another row of 10 cards by clicking on the blue uncovered cards in the bottom right corner. This will add one card to each column. You cannot have an empty column when dealing new cards however." +
-                "\n\nThis is basically it for basic game mechanics, scroll down for button explanation and advanced features. You can play a game with a very easy deck of cards by clicking on the text below\n\n",
-                TextWrapping = TextWrapping.Wrap,
-            };
-            SPInformation.Children.Add(h);
+                "\n\nThis is basically it for basic game mechanics, scroll down for button explanation and advanced features. You can play a game with a very easy deck of cards by clicking on the text below\n\n";
+            SPInformation.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap });
 
             Border border = new()
             {
@@ -181,7 +161,7 @@ namespace Spider_Solitaire
                 Foreground = Brushes.White,
                 Width = 200,
                 Height = 50,
-                Margin = new Thickness(70,8,0,0),
+                Margin = new Thickness(70, 8, 0, 0),
             };
             button.MouseLeftButtonUp += TutorialGame;
             void TutorialGame(object sender, MouseButtonEventArgs e)
@@ -195,19 +175,11 @@ namespace Spider_Solitaire
             }
             border.Child = button;
 
-            TextBlock ButtonExplanation = new()
-            {
-                Text = "Buttons",
-                FontSize = 40
-            };
-            SPInformation.Children.Add(ButtonExplanation);
+            text = "Buttons";
+            SPInformation.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap, FontSize = 40 });
 
-            TextBlock i = new()
-            {
-                Text = "In the game there are 4 buttons, each with their own function.\n",
-                TextWrapping = TextWrapping.Wrap,
-            };
-            SPInformation.Children.Add(i);
+            text = "In the game there are 4 buttons, each with their own function.\n";
+            SPInformation.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap });
 
             StackPanel buttonsOne = new()
             {
@@ -218,24 +190,19 @@ namespace Spider_Solitaire
             {
                 Width = 50,
                 Height = 50,
-                Margin = new Thickness(0,0,20,0),
+                Margin = new Thickness(0, 0, 20, 0),
                 Stretch = Stretch.None,
                 Source = new BitmapImage(new Uri(@"assets/home_icon_pressed.png", UriKind.Relative)),
             };
             buttonsOne.Children.Add(Bimg);
-            TextBlock buttonOneDesc = new()
-            {
-                Height = 80,
-                Width = 275,
-                Text = "This button returns you to the main menu, you can press load game if you wish to continue",
-                TextWrapping = TextWrapping.Wrap,
-            };
-            buttonsOne.Children.Add(buttonOneDesc);
+            text = "This button returns you to the main menu, you can press load game if you wish to continue";
+            buttonsOne.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap, Height = 80, Width = 275 });
+
 
             StackPanel buttonsTwo = new()
             {
                 Orientation = Orientation.Horizontal,
-                Margin = new Thickness(0,10,0,0),
+                Margin = new Thickness(0, 10, 0, 0),
             };
             SPInformation.Children.Add(buttonsTwo);
             Image BTwoimg = new()
@@ -247,14 +214,8 @@ namespace Spider_Solitaire
                 Source = new BitmapImage(new Uri(@"assets/restart_icon_pressed.png", UriKind.Relative)),
             };
             buttonsTwo.Children.Add(BTwoimg);
-            TextBlock buttonTwoDesc = new()
-            {
-                Height = 80,
-                Width = 275,
-                Text = "This button restarts the game from the beggining with the same cards.",
-                TextWrapping = TextWrapping.Wrap,
-            };
-            buttonsTwo.Children.Add(buttonTwoDesc);
+            text = "This button restarts the game from the beggining with the same cards.";
+            buttonsTwo.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap, Height = 50, Width = 275 });
 
 
             StackPanel buttonsThree = new()
@@ -267,19 +228,13 @@ namespace Spider_Solitaire
             {
                 Width = 50,
                 Height = 50,
-                Margin = new Thickness(0, 0, 20, 0),
+                Margin = new Thickness(0, 20, 20, 0),
                 Stretch = Stretch.None,
                 Source = new BitmapImage(new Uri(@"assets/back_icon_pressed.png", UriKind.Relative)),
             };
             buttonsThree.Children.Add(BThreeimg);
-            TextBlock buttonThreeDesc = new()
-            {
-                Height = 30,
-                Width = 275,
-                Text = "This button undos your last move",
-                TextWrapping = TextWrapping.Wrap,
-            };
-            buttonsThree.Children.Add(buttonThreeDesc);
+            text = "This button undos your last move";
+            buttonsThree.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap, Height = 30, Width = 275 });
 
 
             StackPanel buttonsFour = new()
@@ -297,37 +252,22 @@ namespace Spider_Solitaire
                 Source = new BitmapImage(new Uri(@"assets/hint_icon_pressed.png", UriKind.Relative)),
             };
             buttonsFour.Children.Add(BFourimg);
-            TextBlock buttonFourDesc = new()
-            {
-                Height = 100,
-                Width = 275,
-                Text = "This button gives you a hint by highlighting a card or cards that can be picked up and a suggested place to put down.",
-                TextWrapping = TextWrapping.Wrap,
-            };
-            buttonsFour.Children.Add(buttonFourDesc);
+            text = "This button gives you a hint by highlighting a card or cards that can be picked up and a suggested place to put down.";
+            buttonsFour.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap, Height = 100, Width = 275 });
 
-            TextBlock Advanced = new()
-            {
-                Text = "\nAdvanced features and settings",
-                TextWrapping = TextWrapping.Wrap,
-                FontSize = 40
-            };
-            SPInformation.Children.Add(Advanced);
+            text = "\nAdvanced features and settings";
+            SPInformation.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap, FontSize = 40 });
 
-            TextBlock j = new()
-            {
-                Text = "In settings, you can find some things that you can change. You can find the settings button in the top left corner with a ⚙ (gear) symbol on it\n\n" +
-                "Card size determines the scale at which the cards will be rendered, ranging from 50% to 200%, 100% being the default.\n" +
-                "Card spacing determines how far apart will cards be, default is 20 however if you play with 4 suits I is a good idea to increase it to 25 or 30 in order to be able to recognize suits from each other.\n" +
-                "Hint mode affects gameplay. There are 3 modes:\n\n" +
-                "-Enabled (default) - no restrictions\n" +
-                "-Restricted - you have limited number of hints. At the start of the game you have 3 hints and you gain 1 for each assembled suit, together you can get 10 hints (you get 11th when you win)\n" +
-                "-Disabled - You cannot use hints\n\n" +
-                "Additionaly, there are two buttons, one resets all settings to default, the other one erases all statistics\n" +
-                "You need to apply the settings when you change something, otherwise they will not be reflected",
-                TextWrapping = TextWrapping.Wrap,
-            };
-            SPInformation.Children.Add(j);
+            text = "In settings, you can find some things that you can change. You can find the settings button in the top left corner with a ⚙ (gear) symbol on it\n\n" +
+                   "Card size determines the scale at which the cards will be rendered, ranging from 50% to 200%, 100% being the default.\n" +
+                   "Card spacing determines how far apart will cards be, default is 20 however if you play with 4 suits I is a good idea to increase it to 25 or 30 in order to be able to recognize suits from each other.\n" +
+                   "Hint mode affects gameplay. There are 3 modes:\n\n" +
+                   "-Enabled (default) - no restrictions\n" +
+                   "-Restricted - you have limited number of hints. At the start of the game you have 3 hints and you gain 1 for each assembled suit, together you can get 10 hints (you get 11th when you win)\n" +
+                   "-Disabled - You cannot use hints\n\n" +
+                   "Additionaly, there are two buttons, one resets all settings to default, the other one erases all statistics\n" +
+                   "You need to apply the settings when you change something, otherwise they will not be reflected";
+            SPInformation.Children.Add(new TextBlock() { Text = text, TextWrapping = TextWrapping.Wrap });
         }
 
         private void StatsClick(object sender, RoutedEventArgs e)
